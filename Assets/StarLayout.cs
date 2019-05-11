04a5708fd5d8f579c8f90b46e9cbe2359ed11c36
Tony Williams <92tonywills@gmail.com>
2019-05-11 21:59:49 +0100

    Add an example scene with a simple bottom left based star layout

---
+++ Assets/StarLayout.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StarLayout : MonoBehaviour, ILayoutGroup
+{
+    [Header("Star Layout")]
+    [SerializeField] private StarLayoutElement _layoutElementTopLeft;
+    [SerializeField] private StarLayoutElement _layoutElementTopRight;
+    [SerializeField] private StarLayoutElement _layoutElementMiddle;
+    [SerializeField] private StarLayoutElement _layoutElementBottomLeft;
+    [SerializeField] private StarLayoutElement _layoutElementBottomRight;
+
+    public void SetLayoutHorizontal()
+    {
+        SetSizes();
+    }
+
+    private void SetSizes()
+    {
+        SetElementSize(_layoutElementTopLeft);
+        SetElementSize(_layoutElementTopRight);
+        SetElementSize(_layoutElementMiddle);
+        SetElementSize(_layoutElementBottomLeft);
+        SetElementSize(_layoutElementBottomRight);
+    }
+
+    private void SetElementSize(StarLayoutElement element)
+    {
+        element.SetSize(element.preferredWidth, element.preferredHeight);
+    }
+
+    public void SetLayoutVertical()
+    {
+        SetSizes();
+
+        _layoutElementTopLeft.SetBottomLeftPosition(
+            0,
+            RectTransform.rect.height - _layoutElementTopLeft.preferredHeight);
+
+        _layoutElementTopRight.SetBottomLeftPosition(
+            RectTransform.rect.width - _layoutElementTopRight.preferredWidth,
+            RectTransform.rect.height - _layoutElementTopRight.preferredHeight);
+
+        _layoutElementMiddle.SetBottomLeftPosition(
+            RectTransform.rect.width / 2 - _layoutElementMiddle.preferredWidth / 2,
+            RectTransform.rect.height / 2 - _layoutElementMiddle.preferredHeight / 2);
+
+        _layoutElementBottomLeft.SetBottomLeftPosition(
+            0,
+            0);
+
+        _layoutElementBottomRight.SetBottomLeftPosition(
+            RectTransform.rect.width - _layoutElementBottomRight.preferredWidth,
+            0);
+    }
+
+    private RectTransform RectTransform
+    {
+        get { return GetComponent<RectTransform>(); }
+    }
+}
---
+++ Assets/StarLayoutElement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StarLayoutElement : MonoBehaviour, ILayoutElement
+{
+    [Header("Star Layout Element")]
+    [SerializeField] private float _width;
+    [SerializeField] private float _height;
+
+    public float minWidth { get { return _width; } }
+
+    public float preferredWidth { get { return _width; } }
+
+    public float flexibleWidth { get { return 0; } }
+
+    public float minHeight { get { return 0; } }
+
+    public float preferredHeight { get { return _height; } }
+
+    public float flexibleHeight { get { return _height; } }
+
+    public int layoutPriority { get { return 1; } }
+
+    public void CalculateLayoutInputHorizontal()
+    {
+    }
+
+    public void CalculateLayoutInputVertical()
+    {
+    }
+
+    public void SetSize(float width, float height)
+    {
+        RectTransform.sizeDelta = new Vector2(width, height);
+    }
+
+    public void SetBottomLeftPosition(float x, float y)
+    {
+        RectTransform.anchoredPosition =
+            // Align the bottom left of the element
+            new Vector2(RectTransform.rect.width, RectTransform.rect.height) * RectTransform.pivot
+            // With the bottom left of the parent
+            - new Vector2(Parent.rect.width, Parent.rect.height) * RectTransform.pivot
+            // Now place in desired location
+            + new Vector2(x, y);
+    }
+
+    private RectTransform Parent
+    {
+        get { return (RectTransform)RectTransform.parent; }
+    }
+
+    private RectTransform RectTransform
+    {
+        get { return GetComponent<RectTransform>(); }
+    }
+}
